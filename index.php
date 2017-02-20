94d317ccedd621b82a3e701bf55ea9c09e27ac76
Joanmi <Joanmi@DESKTOP-RA7HNA1>
2017-02-20 20:07:02 +0100

    panell d'usuari

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-include("paginas/header.html");
-include("paginas/contenido.html");
-include("paginas/footer.html");
+session_start();
+$pagina = isset($_GET['loc']) ? $_GET['loc'] : 'login';
+
+if($pagina=="principal"){
+    include("paginas/header.html");
+    include("paginas/contenido.html");
+    include("paginas/footer.html");
+}else{
+    include("paginas/index.html");
+}
+
 ?>
