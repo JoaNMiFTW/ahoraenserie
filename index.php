562706b9ee582136b8947428f99d008388ff14d7
Xavier <Xavier@Xavier-PC>
2017-02-21 16:47:04 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
-session_start();
-if (isset($_SESSION["usuari"])){
 
-//if($pagina=="principal"){
+session_start();
+if (isset($_SESSION["usuari"])) {
     include("paginas/header.html");
     include("paginas/contenido.html");
     include("paginas/footer.html");
-}else{
+} else {
     include("paginas/index.html");
 }
-
-
 ?>
--- js/jqueryContenido.js
+++ js/jqueryContenido.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
+    
+    alert("kgkjhgb");
+    
     function carrega_dades() {
-        $.post("comprovar_login.php", function (dades) {
+        $.post("generar_caratula.php", function (dades) {
             mostrar_dades(dades);
         });
     }
 
 
     function mostrar_dades(dades) {
+        $.each(dades,function (i,caratula){
+            $("#contenidorCaratules").append("<div><img src='"+caratula.imagen+"' width='100' height='150'><span>"+caratula.titulo+"</span></div>");
+        });
         
-
     }
+
+    carrega_dades();
 });
